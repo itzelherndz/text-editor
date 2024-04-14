5a77a3d77d4da32395225429d5b1086a1f4fe02c
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-04-13 22:38:50 -0600

    implemented put and get methods in indexedDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,27 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Logic to a method that accepts some content and adds it to the database
+export const putDb = async (id, content) => {
+  console.log('PUT to the database');
+  const textDb = await openDB('jate',1);
+  const tx = textDb.transaction('jate','readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({id: id, text: content});
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET all from the database');
+  const textDb = await openDB('jate',1);
+  const tx = textDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('Result: ', result);
+  return result;
+};
 
 initdb();
