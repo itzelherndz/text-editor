a031855cf6d442fe4a1e852112f267aa7b46c0a4
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-04-13 22:26:49 -0600

    added pwa installation logic

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Text Editor Starter Code
+
+## Credits
+https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/How_to/Trigger_install_prompt
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,29 @@
 const butInstall = document.getElementById('buttonInstall');
-
+let install = null;
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    install = event;
+    butinstall.removeAttribute('hidden');
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (!install){
+        return;
+    }
+    const result = await install.prompt();
+    console.log(`Install prompt was : ${result.outcome}`);
+
+    install = null;
+    butInstall.setAttribute('hidden','');
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    event.preventDefault();
+    install = null;
+    butInstall.setAttribute('hidden','');
+    window.alert('JATE Installed.');
+});
