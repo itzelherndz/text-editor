c5190de2d6dded24df89d8a4621d1e957d089623
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-04-14 10:24:22 -0600

    added stale while revalidate workbox strategy

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,8 +4,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -37,7 +35,9 @@ module.exports = () => {
         public_path: './',
         icons: [
           {
-            src: path.resolve('assets/images/logo.png')
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
           }
         ]
       })  
